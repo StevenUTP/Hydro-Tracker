26cd56e1a3d4c974bfaa4f0fd6c831dabbfb5f65
StevenUTP <stevenspencer0318@gmail.com>
2024-12-10 15:54:23 -0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,160 +1,47 @@
+// src/App.tsx
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Head from "./components/Head";
+import UserForm from "./components/UserForm";
+import UserTable from "./components/UserTable";
+import { Usuario as UsuarioType } from "./Usuario";
 
-// src/components/UserForm.tsx
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Usuario as UsuarioType } from "../Usuario";
+const App: React.FC = () => {
+  const [usuarios, setUsuarios] = useState<UsuarioType[]>([]);
+  const [usuario, setUsuario] = useState<UsuarioType>({} as UsuarioType);
 
-interface UserFormProps {
-    usuarios: UsuarioType[];
-    setUsuarios: React.Dispatch<React.SetStateAction<UsuarioType[]>>;
-    usuario: UsuarioType;
-    setUsuario: React.Dispatch<React.SetStateAction<UsuarioType>>;
-}
-
-const UserForm: React.FC<UserFormProps> = ({ usuarios, setUsuarios, usuario, setUsuario }) => {
-    const [nombre, setNombre] = useState("");
-    const [metaDiaria, setMetaDiaria] = useState(0);
-    const [litrosTomados, setLitrosTomados] = useState(0);
-    const [error, setError] = useState(false);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (Object.keys(usuario).length > 0) {
-            setNombre(usuario.Nombre);
-            setMetaDiaria(usuario.MetaDiaria);
-            setLitrosTomados(usuario.LitrosTomados);
-        }
-    }, [usuario]);
-
-    const generarId = () => {
-        const random = Math.random().toString(36).substr(2);
-        const fecha = Date.now().toString(36);
-        return fecha + random;
+  useEffect(() => {
+    const obtenerLS = () => {
+      const usuariosLS = JSON.parse(localStorage.getItem("usuarios") || "[]");
+      setUsuarios(usuariosLS);
     };
+    obtenerLS();
+  }, []);
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
+  useEffect(() => {
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  }, [usuarios]);
 
-        if ([nombre, metaDiaria, litrosTomados].includes("")) {
-            console.log("Hay campos vacios");
-            setError(true);
-            return;
-        }
-        setError(false);
-
-        const objetoUsuario: UsuarioType = {
-            id: usuario.id || "",
-            Nombre: nombre,
-            MetaDiaria: metaDiaria,
-            LitrosTomados: litrosTomados,
-            LitrosTotales: (usuario.LitrosTotales || 0) + litrosTomados,
-        };
-
-        if (usuario.id) {
-            // editar
-            objetoUsuario.id = usuario.id;
-            const usuariosActualizados = usuarios.map((usuarioState) =>
-                usuarioState.id === usuario.id ? objetoUsuario : usuarioState
-            );
-            setUsuarios(usuariosActualizados);
-            setUsuario({} as UsuarioType);
-        } else {
-            objetoUsuario.id = generarId();
-            setUsuarios([...usuarios, objetoUsuario]);
-        }
-
-        //reiniciar form
-        setNombre("");
-        setMetaDiaria(0);
-        setLitrosTomados(0);
-
-        // Redirigir a la página de UserTable
-        navigate("/users");
-    };
-
-    return (
-        <div className="md:w-1/2 lg:w-2/5 mx-5">
-            <h2 className="font-black text-3xl text-center">Formulario</h2>
-
-            <p className="text-lg mt-5 text-center mb-10">
-                Añade usuarios y{" "}
-                <span className="text-indigo-600 font-bold ">Adminístralos</span>
-            </p>
-
-            <form
-                onSubmit={handleSubmit}
-                action=""
-                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
-            >
-
-                <div className="mb-5">
-                    <label
-                        htmlFor="nombre"
-                        className="block text-gray-700 uppercase font-bold"
-                    >
-                        Nombre
-                    </label>
-
-                    <input
-                        id="nombre"
-                        type="text"
-                        placeholder="Nombre del usuario"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        value={nombre}
-                        onChange={(e) => {
-                            setNombre(e.target.value);
-                        }}
-                    />
-                </div>
-
-                <div className="mb-5">
-                    <label
-                        htmlFor="metaDiaria"
-                        className="block text-gray-700 uppercase font-bold"
-                    >
-                        Meta Diaria (Litros)
-                    </label>
-
-                    <input
-                        id="metaDiaria"
-                        type="number"
-                        placeholder="Meta diaria en litros"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        value={metaDiaria}
-                        onChange={(e) => {
-                            setMetaDiaria(Number(e.target.value));
-                        }}
-                    />
-                </div>
-
-                <div className="mb-5">
-                    <label
-                        htmlFor="litrosTomados"
-                        className="block text-gray-700 uppercase font-bold"
-                    >
-                        Litros Tomados
-                    </label>
-
-                    <input
-                        id="litrosTomados"
-                        type="number"
-                        placeholder="Litros tomados"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        value={litrosTomados}
-                        onChange={(e) => {
-                            setLitrosTomados(Number(e.target.value));
-                        }}
-                    />
-                </div>
-
-                <input
-                    type="submit"
-                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-                    value={usuario.id ? "Guardar Cambios" : "Agregar Usuario"}
-                />
-            </form>
-        </div>
+  const eliminarUsuario = (id: string) => {
+    const usuariosActualizados = usuarios.filter(
+      (usuario) => usuario.id !== id
     );
+    setUsuarios(usuariosActualizados);
+  };
+
+  return (
+    <Router>
+      <div className="container mx-auto mt-20">
+        <Head />
+        <div className="mt-12 md:flex">
+          <Routes>
+            <Route path="/" element={<UserForm usuarios={usuarios} setUsuarios={setUsuarios} usuario={usuario} setUsuario={setUsuario} />} />
+            <Route path="/users" element={<UserTable usuarios={usuarios} setUsuarios={setUsuarios} setUsuario={setUsuario} eliminarUsuario={eliminarUsuario} />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 };
 
-export default UserForm;
+export default App;
